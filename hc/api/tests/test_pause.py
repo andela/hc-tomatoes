1bf61663097a2a40914d77753a078ae9ef17941f
dennisja <dennisjjagwe@gmail.com>
2018-01-07 09:24:49 +0300

    [Ft #153112182] Fix api tests in test_pause.py Add missing dependancies to requirements.txt Ensure that 200 is returned when check is paused and the check status changes to paused Test whether only post requests are allowed when pausing a check

--- hc/api/tests/test_pause.py
+++ hc/api/tests/test_pause.py
@@ -13,6 +13,11 @@ class PauseTestCase(BaseTestCase):
                              HTTP_X_API_KEY="abc")
 
         ### Assert the expected status code and check's status
+        # test if the request was successful
+        self.assertEqual(r.status_code, 200)
+        # test if the check status changed to paused
+        check.refresh_from_db()
+        self.assertEqual(check.status, "paused")
 
     def test_it_validates_ownership(self):
         check = Check(user=self.bob, status="up")
@@ -25,3 +30,11 @@ class PauseTestCase(BaseTestCase):
         self.assertEqual(r.status_code, 400)
 
         ### Test that it only allows post requests
+    def test_it_only_allows_post_requests(self):
+        check = Check(user=self.alice, status="up")
+        check.save()
+
+        url = f"/api/v1/checks/{check.code}/pause"
+        r = self.client.get(url, HTTP_X_API_KEY="abc")
+        self.assertEqual(r.status_code, 405)
+
